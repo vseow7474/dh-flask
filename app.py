ec1e73c7097a63fb6b63cad421e10352133421bc
vseow7474 <vseow7474@gmail.com>
2025-02-13 21:47:57 +0800

    changes to app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Victor Seow!</p>"
+    return "<p>Hello, Victor!</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
